88c73bdfd3d12931092fac553d0182c986a90deb
Milan-Uy <uymillano@gmail.com>
2024-04-18 11:45:38 +0800

    Routing and folder structure

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UpdateTransformationPage = () => {
+  return <div>UpdateTransformationPage</div>;
+};
+
+export default UpdateTransformationPage;
+
+// localhost:3000/transformations/[id]/update
