35693686ce0ab7f3a7553e2b86b1a6927a1944de
Milan-Uy <uymillano@gmail.com>
2024-05-06 23:22:43 +0800

    clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { IBM_Plex_Sans } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const IBMPlex = IBM_Plex_Sans({ subsets: ['latin'], weight: ['400', '500', '600', '700'], variable: '--font-ibm-plex' });
 
@@ -16,9 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      {/* IBMPlex.variable applies the font while antialiased makes the text more readable */}
-      <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: '#624cf5' },
+      }}
+    >
+      <html lang='en'>
+        {/* IBMPlex.variable applies the font while antialiased makes the text more readable */}
+        <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
