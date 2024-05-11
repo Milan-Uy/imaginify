801c7f340ca12b8058b810979ae9d9117662177f
Milan-Uy <uymillano@gmail.com>
2024-05-11 21:55:18 +0800

    layout sidebar and mobile navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <p>Home</p>
+      {/* <p>Home</p>
 
-      <UserButton afterSignOutUrl='/' />
+      <UserButton afterSignOutUrl='/' /> */}
     </div>
   );
 };
